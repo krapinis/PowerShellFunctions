eba590b5f75bdde4d6ae651e564d94a12ddc3f35
Mantas Zinkevicius <mantas.zinkevicius@hotmail.com>
2018-07-26 16:22:28 +0200

    initial commit

---
+++ Finctions-start.ps1
@@ -0,0 +1,25 @@
+#https://4sysops.com/archives/the-powershell-function-parameters-data-types-return-values/
+#https://en.wikiversity.org/wiki/PowerShell/Functions
+#https://blogs.technet.microsoft.com/heyscriptingguy/2011/05/22/use-powershell-to-make-mandatory-parameters/
+
+#function [<scope:>]<name> [([type]$parameter1[,[type]$parameter2])]{
+ 
+#    param([type]$parameter1 [,[type]$parameter2])
+#    dynamicparam {<statement list>}
+ 
+#    begin {<statement list>}
+#    process {<statement list>}
+#    end {<statement list>}
+ 
+#}
+
+function MyPing
+{
+    Param(
+        # Parameter help description
+        [parameter(Mandatory = $true)]
+        [String]
+        $ParameterName
+    )
+}
+$Script:x=0
